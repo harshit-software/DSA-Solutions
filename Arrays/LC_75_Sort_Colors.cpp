33205f29ad9ed61ebe241531052b6e1a1b72f3ca
Harshit Agrawal <harshit.agr.1511@gmail.com>
2026-02-19 15:11:16 +0530

    Added

---
+++ Arrays/LC_75_Sort_Colors.cpp
@@ -0,0 +1,41 @@
+// Method 1
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size(), low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+// Method 2
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size(),count0=0,count1=0,count2=0;
+        for(auto it:nums){
+            if(it==0) count0++;
+            else if (it==1) count1++;
+            else count2++;
+        }
+        for(int i=0;i<count0;i++){
+            nums[i]=0;
+        }
+        for(int i=count0;i<count0+count1;i++){
+            nums[i]=1;
+        }
+        for(int i=count0+count1;i<n;i++){
+            nums[i]=2;
+        }
+    }
+};
