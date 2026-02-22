92ec1e336260320a40211e1aaff2dc9c2c7d05a0
Harshit Agrawal <harshit.agr.1511@gmail.com>
2026-02-22 12:43:24 +0530

    Added Solutions

---
+++ Arrays/LC_2678_Number of Senior Citizens.cpp
@@ -0,0 +1,12 @@
+// Method 1
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0,n=details.size();
+        for(int i=0;i<n;i++){
+            int sum=int(details[i][11]-'0')*10+int(details[i][12]-'0');
+            if(sum>60) count++;
+        }
+        return count;
+    }
+};
--- Arrays/LC_268_Missing Number.cpp
+++ Arrays/LC_268_Missing Number.cpp
@@ -11,3 +11,17 @@ public:
         return total-sum;
     }
 };
+
+// Method 2
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            ans= ans^i;
+        }
+        for(auto it:nums) ans^=it;
+        return ans;
+    }
+};
