f13d982300fa483fb788f06d3a80d6868bef361b
Harshit Agrawal <harshit.agr.1511@gmail.com>
2026-02-22 12:43:45 +0530

    Added Solutions

--- Arrays/LC_2678_Number of Senior Citizens.cpp
+++ Arrays/LC_2678_Number of Senior Citizens.cpp
@@ -10,3 +10,18 @@ public:
         return count;
     }
 };
+
+// Method 2
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        
+        for (auto &d : details) {
+            int age = stoi(d.substr(11, 2));
+            if (age > 60) count++;
+        }
+        
+        return count;
+    }
+};
