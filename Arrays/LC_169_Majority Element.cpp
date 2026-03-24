b51ab2a93529b1f6553ef1919fbf13b3f3db7f2a
Harshit Agrawal <harshit.agr.1511@gmail.com>
2026-03-24 09:59:01 +0530

    Added Optimised Solution

--- Arrays/LC_169_Majority Element.cpp
+++ Arrays/LC_169_Majority Element.cpp
@@ -1,3 +1,20 @@
+// Bruteforce Approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]) count++;
+                if(count>=n/2) return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+// Better Approach
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -8,5 +25,19 @@ public:
             if(mp[it]>n/2) return it;
         }
         return 1;
+    }
+};
+
+// Optimal Approach (Moore's Voting Algorithm)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size(),count=0,ele;
+        for(int i=0;i<n;i++){
+            if(count==0) ele=nums[i];
+            if(ele==nums[i]) count++;
+            else count--;
+        }
+        return ele;
     }
 };
