44be84fe174b660341406d6f1ae3f2142c8eac48
Harshit Agrawal <harshit.agr.1511@gmail.com>
2026-04-10 21:39:49 +0530

    Added Solutoin

---
+++ Arrays/LC_1952_Three Divisors.cpp
@@ -0,0 +1,28 @@
+// Bruteforce Approach
+class Solution {
+public:
+    bool isThree(int n) {
+        if(n<=2) return false;
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0) count++;
+        }
+        if(count==3) return true;
+        return false;
+    }
+};
+
+// Optimal Approach
+class Solution {
+public:
+    bool isThree(int n) {
+        if(n<=2) return false;
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0) count++;
+            if(count>3) return false;
+        }
+        if(count==3) return true;
+        return false;
+    }
+};
